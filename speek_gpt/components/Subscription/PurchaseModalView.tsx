fd4264e993a4d05bccc9923ebfc1f710fc47eb73
sasakitimaru <84312254+sasakitimaru@users.noreply.github.com>
2023-05-27 17:14:49 +0900

    スクローViewに変更

--- speek_gpt/components/Subscription/PurchaseModalView.tsx
+++ speek_gpt/components/Subscription/PurchaseModalView.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { StyleSheet, View, Modal, TouchableOpacity, Text, Linking } from 'react-native';
+import { StyleSheet, View, Modal, TouchableOpacity, Text, Linking, ScrollView } from 'react-native';
 import { Iconify } from 'react-native-iconify';
 import { ModalVisibleContext } from '../../App';
 import PremiumpPlanBox from './components/PremiumPlanBpx';
@@ -14,11 +14,11 @@ type ModalVisibleContextProps = {
 const PurchaseModalView: React.FC = () => {
     const { modalVisible, setModalVisible } = useContext<ModalVisibleContextProps>(ModalVisibleContext);
     const [isPlanPremium, setIsPlanPremium] = useState<boolean>(true);
-    const [termsvisible , setTermsVisible] = useState<boolean>(false);
+    const [termsvisible, setTermsVisible] = useState<boolean>(false);
 
     const handlecancelSubscriptions = async () => {
         deepLinkToSubscriptionsIos();
-      };
+    };
 
     const handleEULA = () => {
         Linking.openURL('https://sasakitimaru.github.io/React_VoiceRecog_App/EULA');
@@ -29,43 +29,45 @@ const PurchaseModalView: React.FC = () => {
             animationType="slide"
             presentationStyle='formSheet'
         >
-            <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
-                <Iconify style={{ padding: 15, margin: 10 }} icon={"material-symbols:close"} color={'black'} />
-            </TouchableOpacity>
-            <View style={styles.container}>
-                {isPlanPremium ?
-                    <Text style={styles.header}>{`ほぼネイティブスピーカーの\n発音で英会話をする`}</Text>
-                    : <Text style={styles.header}>{`いつでもあなたのそばにいます。\nAIと英会話を楽しもう。`}</Text>
-                }
-                {isPlanPremium ?
-                    <Text style={styles.comment}>ネイティブ読み上げ機能を使うと、より自然な音声で文章を読み上げます。プレミアムプランならネイティブ読み上げを30000文字/月、通常読み上げを50000文字/月まで使用できます</Text>
-                    : <Text style={styles.comment}>{`スタンダードプランでは通常の読み上げモードで30000文字/月での使用が可能になります。スタンダードプランならネイティブ読み上げを1000文字/月、通常読み上げを50000文字/月まで使用できます`}</Text>
-                }
-                {isPlanPremium ? <PremiumpPlanBox /> : <StandardPlanBox />}
-                <TouchableOpacity
-                    style={styles.toStandardPlan}
-                    onPress={() => setIsPlanPremium(!isPlanPremium)}>
-                    {isPlanPremium ? <Text style={{ color: '#136FFF' }}>スタンダードプラン</Text> : <Text style={{ color: '#FF367F' }}>プレミアムプラン</Text>}
-                    <Iconify icon={"ic:sharp-arrow-right"} color={'black'} />
+            <ScrollView>
+                <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
+                    <Iconify style={{ padding: 15, margin: 10 }} icon={"material-symbols:close"} color={'black'} />
                 </TouchableOpacity>
-                <Text style={styles.termsOfService}>購読権はiTunesのアカウントで、次の支払い時期の24時間前に購読キャンセルするまで自動的に支払いが行われます。iTunesアカウントの設定に進み、自動支払いの更新を管理できます。購入が完了するとiTunesアカウントでお支払いが行われます。会員登録を行うことで、個人情報保護方針及びサービス条件に同意することとみなされます。</Text>
-                {/* <TouchableOpacity
+                <View style={styles.container}>
+                    {isPlanPremium ?
+                        <Text style={styles.header}>{`ほぼネイティブスピーカーの\n発音で英会話をする`}</Text>
+                        : <Text style={styles.header}>{`いつでもあなたのそばにいます。\nAIと英会話を楽しもう。`}</Text>
+                    }
+                    {isPlanPremium ?
+                        <Text style={styles.comment}>ネイティブ読み上げ機能を使うと、より自然な音声で文章を読み上げます。プレミアムプランならネイティブ読み上げを30000文字/月、通常読み上げを50000文字/月まで使用できます</Text>
+                        : <Text style={styles.comment}>{`スタンダードプランでは通常の読み上げモードで30000文字/月での使用が可能になります。スタンダードプランならネイティブ読み上げを1000文字/月、通常読み上げを50000文字/月まで使用できます`}</Text>
+                    }
+                    {isPlanPremium ? <PremiumpPlanBox /> : <StandardPlanBox />}
+                    <TouchableOpacity
+                        style={styles.toStandardPlan}
+                        onPress={() => setIsPlanPremium(!isPlanPremium)}>
+                        {isPlanPremium ? <Text style={{ color: '#136FFF' }}>スタンダードプラン</Text> : <Text style={{ color: '#FF367F' }}>プレミアムプラン</Text>}
+                        <Iconify icon={"ic:sharp-arrow-right"} color={'black'} />
+                    </TouchableOpacity>
+                    <Text style={styles.termsOfService}>購読権はiTunesのアカウントで、次の支払い時期の24時間前に購読キャンセルするまで自動的に支払いが行われます。iTunesアカウントの設定に進み、自動支払いの更新を管理できます。購入が完了するとiTunesアカウントでお支払いが行われます。会員登録を行うことで、個人情報保護方針及びサービス条件に同意することとみなされます。</Text>
+                    {/* <TouchableOpacity
                     onPress={() => { setTermsVisible(true); }}
                 >
                     <Text style={styles.termsOfSerciveLink}>利用規約</Text>
                 </TouchableOpacity> */}
-                <TouchableOpacity
-                    onPress={handleEULA}
-                >
-                    <Text style={styles.termsOfSerciveLink}>利用規約/プライバシーポリシー</Text>
-                </TouchableOpacity>
-                <TermsOfSerciveModal visible={termsvisible} setVisible={setTermsVisible}/>
-                <TouchableOpacity
-                onPress={handlecancelSubscriptions}
-                >
-                    <Text style={styles.cancellation}>解約</Text>
-                </TouchableOpacity>
-            </View>
+                    <TouchableOpacity
+                        onPress={handleEULA}
+                    >
+                        <Text style={styles.termsOfSerciveLink}>利用規約/プライバシーポリシー</Text>
+                    </TouchableOpacity>
+                    <TermsOfSerciveModal visible={termsvisible} setVisible={setTermsVisible} />
+                    <TouchableOpacity
+                        onPress={handlecancelSubscriptions}
+                    >
+                        <Text style={styles.cancellation}>解約</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
         </Modal>
     );
 };
